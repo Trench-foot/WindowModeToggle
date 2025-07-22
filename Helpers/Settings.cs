038f453ddce73926745ed19080b2fe5958f91241
Trench-foot <jamesodell66@gmail.com>
2025-07-22 11:29:48 -0500

    Initial Commit/Release
    Simple mod that allows you to keybind the window setting of SPT

---
+++ Helpers/Settings.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx.Configuration;
+using UnityEngine;
+
+namespace WindowModeToggle.Helpers
+{
+    internal enum ScreenMode
+    {
+        ExclusiveFullScreen,
+        FullScreenWindow,
+        MaximizedWindow,
+        Windowed
+    }
+    internal enum ModifierKeyOptions
+    {
+        Alt,
+        Shift,
+        Control
+    }
+
+    internal class Settings
+    {
+        private const string MainScreenModeSetting = "A. Main window mode.";
+        private const string AltScreenModeSetting = "B. Secondary window mode";
+        private const string WindowToggleKeySetting = "C. Window toggle mode key";
+        private const string ModifierKeySettings = "D. Toggle to require use of modifier key";
+
+        public static ConfigEntry<ScreenMode> MainScreenModeToggle { get; set; }
+        public static ConfigEntry<ScreenMode> AltScreenModeToggle { get; set; }
+        public static ConfigEntry<KeyboardShortcut> WindowToggleKey;
+        public static ConfigEntry<bool> ModifierKeyToggle { get; set; }
+        public static ConfigEntry<ModifierKeyOptions> ModifierKey;
+
+
+        public static void Init(ConfigFile Config)
+        {
+            MainScreenModeToggle = Config.Bind(
+                MainScreenModeSetting,
+                "Primary screen mode",
+                ScreenMode.Windowed,
+                "Screen mode setting number 1."
+            );
+
+            AltScreenModeToggle = Config.Bind(
+                AltScreenModeSetting,
+                "Secondary screen mode",
+                ScreenMode.MaximizedWindow,
+                "Screen mode setting number 2."
+            );
+
+            WindowToggleKey = Config.Bind(
+                WindowToggleKeySetting,
+                "1. Toggle Window mode",
+                new KeyboardShortcut(KeyCode.F9),
+                "Key to toggle between window modes dynamically."
+            );
+
+            ModifierKeyToggle = Config.Bind(
+                ModifierKeySettings,
+                "2. Toggle usage of a modifier key",
+                false,
+                "Key to modify activation of custom hotkeys."
+            );
+
+            ModifierKey = Config.Bind(
+                ModifierKeySettings,
+                "3. Modifier key",
+                ModifierKeyOptions.Alt,
+                "Hold this key to use with toggle key."
+            );
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,127 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using WindowModeToggle.Helpers;
+
+namespace WindowModeToggle
+{
+    // first string below is your plugin's GUID, it MUST be unique to any other mod. Read more about it in BepInEx docs. Be sure to update it if you copy this project.
+    [BepInPlugin("WindowModeToggle", "WindowModeToggle", "1.0.0")]
+    [BepInDependency("com.SPT.core", "3.11.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LogSource;
+        // Test the current screen mode of tarkov
+        private bool testScreenModeSettings()
+        {
+            FullScreenMode _fullScreenMode = GetFullScreenMode();
+            
+            if (_fullScreenMode == (FullScreenMode)Settings.MainScreenModeToggle.Value)
+            {
+                //Logger.LogInfo(_fullScreenMode + "false");
+                return false;
+            }
+            else if (_fullScreenMode == (FullScreenMode)Settings.AltScreenModeToggle.Value)
+            {
+                //Logger.LogInfo(_fullScreenMode + "true");
+                return true;
+            }
+            //Special use case, windows doesn't support Maximized window, but I couldn't just remove it
+            // so this was born
+            else if(_fullScreenMode == (FullScreenMode)ScreenMode.FullScreenWindow)
+            {
+                if(ScreenMode.MaximizedWindow == Settings.MainScreenModeToggle.Value)
+                {
+                    return false;
+                }
+                else if(ScreenMode.MaximizedWindow == Settings.AltScreenModeToggle.Value)
+                {
+                    return true;
+                }
+                return true;
+            }
+            else
+            {
+                //Logger.LogInfo(_fullScreenMode + "false");
+                return false;
+            }
+        }
+        // Test for Modifier key binding being presssed and maps the options to the 
+        // left and right versions of it
+        private bool testModifierKeyBind()
+        {
+            if(Settings.ModifierKey.Value == ModifierKeyOptions.Alt)
+            {
+                if(Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
+                {
+                    return true;
+                }
+            }
+            else if(Settings.ModifierKey.Value == ModifierKeyOptions.Shift)
+            {
+                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                {
+                    return true;
+                }
+            }
+            else if(Settings.ModifierKey.Value == ModifierKeyOptions.Control)
+            {
+                if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        // Gets the current screen mode of tarkov
+        private FullScreenMode GetFullScreenMode()
+        {
+            //Logger.LogInfo(Screen.fullScreenMode);
+            return Screen.fullScreenMode;
+        }
+        // BaseUnityPlugin inherits MonoBehaviour, so you can use base unity functions like Awake() and Update()
+        private void Awake()
+        {
+            Settings.Init(Config);
+            LogSource = Logger;
+            LogSource.LogInfo("plugin loaded!");
+        }
+        public void Update()
+        {
+            HandleInput();
+        }
+        // Handles checking for matching input and toggles the screen mode as per settings.
+        private void HandleInput()
+        {
+            if (Settings.ModifierKeyToggle.Value)
+            {
+                if (testModifierKeyBind())
+                {
+                    if (Input.GetKeyDown(Settings.WindowToggleKey.Value.MainKey) && testScreenModeSettings())
+                    {
+                        Logger.LogInfo(GetFullScreenMode() + "false");
+                        GameGraphicsClass.smethod_2((FullScreenMode)Settings.MainScreenModeToggle.Value);
+                    }
+                    else if (Input.GetKeyDown(Settings.WindowToggleKey.Value.MainKey) && !testScreenModeSettings())
+                    {
+                        Logger.LogInfo(GetFullScreenMode() + "true");
+                        GameGraphicsClass.smethod_2((FullScreenMode)Settings.AltScreenModeToggle.Value);
+                    }
+                }
+            }
+            else if (!Settings.ModifierKeyToggle.Value)
+            {
+                if (Input.GetKeyDown(Settings.WindowToggleKey.Value.MainKey) && testScreenModeSettings())
+                {
+                    Logger.LogInfo(GetFullScreenMode() + "false");
+                    GameGraphicsClass.smethod_2((FullScreenMode)Settings.MainScreenModeToggle.Value);
+                }
+                else if (Input.GetKeyDown(Settings.WindowToggleKey.Value.MainKey) && !testScreenModeSettings())
+                {
+                    Logger.LogInfo(GetFullScreenMode() + "true");
+                    GameGraphicsClass.smethod_2((FullScreenMode)Settings.AltScreenModeToggle.Value);
+                }
+            }
+        }
+    }
+}
